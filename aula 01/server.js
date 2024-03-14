530d10fdef5ee6631ccaa6b8796af693b45c2f4d
Matheus <matheusnatanael.oficial@gmail.com>
2024-03-14 10:00:22 -0300

    Atualização da aula 01

--- aula 01/server.js
+++ aula 01/server.js
@@ -2,10 +2,18 @@ import http from "http";
 
 const PORT = 3000;
 
+// http://localhost:3000/
+
+const rotas = {
+  "/":"Rota Home!",
+  "/livros":"Rota livros!",
+  "/autores":"Rota autores"
+}
+
 // Create a local server to receive data from
 const server = http.createServer((req,res) =>{
     res.writeHead(200,{'content-type':'text/plan'});
-    res.end('Hello World!');
+    res.end(rotas[req.url]);
 });
 
 // Listen a local server
